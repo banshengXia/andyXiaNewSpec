94c5c1d913aa732fdd652ebfceb03fd6b3a61155
AndyXia <andyxiamac@AndyXiadeMacBook-Pro.local>
2018-02-09 11:06:34 +0800

    Initial commit

---
+++ Example/MyLib/XIAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XIAAppDelegate.h
+//  MyLib
+//
+//  Created by banshengXia on 02/09/2018.
+//  Copyright (c) 2018 banshengXia. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XIAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/MyLib/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyLib
+//
+//  Created by banshengXia on 02/09/2018.
+//  Copyright (c) 2018 banshengXia. All rights reserved.
+//
+
+@import UIKit;
+#import "XIAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XIAAppDelegate class]));
+    }
+}
