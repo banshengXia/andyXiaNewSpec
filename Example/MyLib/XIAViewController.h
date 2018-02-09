9087d2c792ad6cd83750093f262ea541b2998e68
AndyXia <597374548@qq.com>
2018-02-09 15:48:33 +0800

    初始化

---
+++ Example/MyLib/XIAViewController.h
@@ -0,0 +1,13 @@
+//
+//  XIAViewController.h
+//  MyLib
+//
+//  Created by banshengXia on 02/09/2018.
+//  Copyright (c) 2018 banshengXia. All rights reserved.
+//
+
+@import UIKit;
+
+@interface XIAViewController : UIViewController
+
+@end
---
+++ Example/MyLib/XIAViewController.m
@@ -0,0 +1,29 @@
+//
+//  XIAViewController.m
+//  MyLib
+//
+//  Created by banshengXia on 02/09/2018.
+//  Copyright (c) 2018 banshengXia. All rights reserved.
+//
+
+#import "XIAViewController.h"
+
+@interface XIAViewController ()
+
+@end
+
+@implementation XIAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
